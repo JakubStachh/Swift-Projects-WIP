49363e99cf56b3b056936fe00a2b739e1f83b327
Jakub🐕 <166159581+JakubStachh@users.noreply.github.com>
2025-04-04 12:09:10 +0200

    Update and rename Find the Most Frequent Element in an Array.swift to FindtheMostFrequentElementinanArray.swift

--- Swift Projects/Find the Most Frequent Element in an Array/Find the Most Frequent Element in an Array.swift
+++
@@ -1,5 +0,0 @@
-func mostFrequent(_ arr: [Int]) -> Int? {
-    let counts = arr.reduce(into: [:]) { $0[$1, default: 0] += 1 }
-    return counts.max(by: { $0.value < $1.value })?.key
-}
-print(mostFrequent([1, 2, 2, 3, 3, 3, 4])) // Output: 3
